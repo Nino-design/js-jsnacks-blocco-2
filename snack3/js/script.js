da5e86639c120a9e115523a131ae4880c8ce93c4
Nino-design <npompili@gmail.com>
2022-04-20 16:24:24 +0100

    terzo snack

---
+++ snack3/js/script.js
@@ -0,0 +1,9 @@
+/*jsnack3. Stampa il cubo dei primi N numeri, dove N è un numero indicato dall'utente.*/
+
+const number = parseInt(prompt('Dimmi un numero qualsiasi'));
+
+
+for( let i =0; i <= number; i++){
+    console.log(i * i * i);
+}
+
